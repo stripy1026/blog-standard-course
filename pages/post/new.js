f56ba34575bcf2e521775167d81bd8d57cebea62
stripy1026 <aequatio1026@gmail.com>
2023-05-10 16:00:40 +0900

    feat: :sparkles: Add ChatGPT-3.5-turbo

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,11 +1,56 @@
+import { useState } from "react";
+
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLayout";
 
 export default function NewPost(props) {
-  console.log("NEW POST PROPS:", props);
+  const [topic, setTopic] = useState("");
+  const [keywords, setKeywords] = useState("");
+  const [postContent, setPostContent] = useState("AI is working ...");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`/api/generatePost`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ topic, keywords }),
+    });
+    const json = await response.json();
+    setPostContent(json.post.postContent);
+  };
   return (
     <>
-      <h1>This is new</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <strong>Generate a blog post on the topic of:</strong>
+          </label>
+          <textarea
+            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>
+            <strong>Targeting the following keywords:</strong>
+          </label>
+          <textarea
+            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+            value={keywords}
+            onChange={(e) => setKeywords(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <div
+        className="max-w-screen-sm p-10"
+        dangerouslySetInnerHTML={{ __html: postContent }}
+      />
     </>
   );
 }
